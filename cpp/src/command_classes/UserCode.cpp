koush/open-zwave

commit 12a30b9f87e8b3cabec45db313c0db689453bb49

Fix Windows Build Failure

cpp/src/command_classes/UserCode.cpp
@@ -511,7 +511,7 @@ bool UserCode::HandleMsg
 					node->CreateValueString( ValueID::ValueGenre_User, GetCommandClassId(), _instance, i, str, "", false, false, data, 0 );
 				}
 				m_userCode[i].status = UserCode_Available;
-				bzero(&m_userCode[i].usercode, 10);
+				memcpy(&m_userCode[i].usercode, 0, 10);
 			}
 			if (m_exposeRawValueID) {
 				node->CreateValueRaw( ValueID::ValueGenre_User, GetCommandClassId(), _instance, UserCodeIndex_RawValue, "Raw UserCode", "", false, false, 0, 0, 0);

commit 2807a1be2361ab2fb761701102dc819065609ddc

Fix null pointer reference caused by 12a30b9f87e8b3cabec45db313c0db689453bb49

cpp/src/command_classes/UserCode.cpp
@@ -511,7 +511,7 @@ bool UserCode::HandleMsg
 					node->CreateValueString( ValueID::ValueGenre_User, GetCommandClassId(), _instance, i, str, "", false, false, data, 0 );
 				}
 				m_userCode[i].status = UserCode_Available;
-				memcpy(&m_userCode[i].usercode, 0, 10);
+				memset(&m_userCode[i].usercode, 0, 10);
 			}
 			if (m_exposeRawValueID) {
 				node->CreateValueRaw( ValueID::ValueGenre_User, GetCommandClassId(), _instance, UserCodeIndex_RawValue, "Raw UserCode", "", false, false, 0, 0, 0);